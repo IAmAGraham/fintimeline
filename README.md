683b9524c15e801efeae4a357850d502c1e54b82
github username <github email address>
2017-09-28 14:27:26 -0400

    details in readme and add alphavantage to node package

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # fintimeline
+
+A site dedicated to charting out various equities using the https://www.alphavantage.co/ API
+
+chart could potentially use https://github.com/rrag/react-stockcharts
--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import ReactDOM from 'react-dom';
 
 import NavBar from './components/NavBar';
 
+
+const alpha = require('alphavantage')({ key: 'MWQ2U4K8HU6D7VUK' })
+// const API_Key = 'MWQ2U4K8HU6D7VUK'
+
 ReactDOM.render(NavBar({title: 'FinTimeline', style: 'inverse'}), document.getElementById('root'))
